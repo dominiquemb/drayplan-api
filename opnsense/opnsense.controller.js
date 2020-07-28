d9960bcc575f69ac7043837630afd896e2433890
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2020-07-28 18:34:06 +0000

    Adding ?hidemenu=true to opnsense route

--- opnsense/opnsense.controller.js
+++ opnsense/opnsense.controller.js
@@ -6,6 +6,13 @@ const rp = require('request-promise');
 router.get('/getRoutes', getRoutes);
 router.get('/serverStatus', serverStatus);
 router.get('/getUsers', getUsers);
+router.get('/getFirewallRules', getFirewallRules);
+router.get('/getSingleGateways', getSingleGateways);
+router.post('/editSingleGateway', editSingleGateway);
+router.get('/editSingleGateway', editSingleGatewayForm);
+router.post('/editOpnSenseItem', editOpnSenseItem);
+router.get('/editOpnSenseItemForm', editOpnSenseItemForm);
+router.get('/viewOpnSensePage', viewOpnSensePage);
 router.get('/getInterfaces', getInterfaces);
 router.get('/getInterface', getInterface);
 router.post('/editInterface', editInterface);
@@ -99,6 +106,83 @@ async function getUsers(req, res, next) {
 		});
 }
 
+async function getSingleGateways(req, res, next) {
+	let options = {};
+	options.method = 'GET';
+	options.uri = customHeaders.uri + '/system_getgateways.php';
+	//options.headers = baseHeaders.headers;
+	await rp(options)
+		.then(function(resp) {
+			let data = JSON.parse(resp);
+			let array = [];
+			for(let [index, gw] of Object.entries(data)) {
+				let gwObject = { id: parseInt(index), ...gw };	
+				array.push(gwObject);
+			}
+			res.json(array);
+		})
+		.catch(function(err) {
+			res.status(400).json({ message: err })
+		});
+}
+
+async function getFirewallRules(req, res, next) {
+	let options = {};
+	let { query } = req;
+	options.method = 'GET';
+	options.uri = customHeaders.uri + '/firewall_getrules.php';
+	if (query) {
+		if (query['if']) {
+			options.uri += '?if=' + query['if'];
+		}
+	}
+	//options.headers = baseHeaders.headers;
+	await rp(options)
+		.then(function(resp) {
+			let data = JSON.parse(resp);
+			res.json(data);
+		})
+		.catch(function(err) {
+			res.status(400).json({ message: err })
+		});
+}
+
+async function getSingleGateways(req, res, next) {
+	let options = {};
+	options.method = 'GET';
+	options.uri = customHeaders.uri + '/system_getgateways.php';
+	//options.headers = baseHeaders.headers;
+	await rp(options)
+		.then(function(resp) {
+			let data = JSON.parse(resp);
+			res.json(data);
+		})
+		.catch(function(err) {
+			res.status(400).json({ message: err })
+		});
+}
+
+async function getFirewallRules(req, res, next) {
+	let options = {};
+	let { query } = req;
+	options.method = 'GET';
+	options.uri = customHeaders.uri + '/firewall_getrules.php';
+	if (query) {
+		if (query['if']) {
+			options.uri += '?if=' + query['if'];
+		}
+	}
+	//options.headers = baseHeaders.headers;
+	await rp(options)
+		.then(function(resp) {
+			let data = JSON.parse(resp);
+			res.json(data);
+		})
+		.catch(function(err) {
+			res.status(400).json({ message: err })
+		});
+}
+
 async function getInterfaces(req, res, next) {
 	let options = {};
 	options.method = 'GET';
@@ -140,8 +224,159 @@ async function getInterface(req, res, next) {
 		});
 }
 
+async function editSingleGateway(req, res, next) {
+	let options = {};
+	let { query } = req;
+	let { id } = query; 
+	
+	options.method = 'POST';
+	if (id) {
+		options.uri = customHeaders.uri + `/system_gateways_edit_trigger.php?id=${id}`;
+	} else {
+		options.uri = customHeaders.uri + `/system_gateways_edit_trigger.php`;
+	}
+	options.formData = req.body;
+	await rp(options)
+		.then(function(resp) {
+			res.json(JSON.parse(resp));
+		})
+		.catch(function(err) {
+			res.status(400).json({ message: err })
+		});
+}
+
+async function editSingleGatewayForm(req, res, next) {
+	let options = {};
+	let { query } = req;
+	let { id } = query; 
+	
+	options.method = 'GET';
+	if (id) {
+		options.uri = customHeaders.uri + `/system_gateways_edit_trigger.php?id=${id}`;
+	} else {
+		options.uri = customHeaders.uri + `/system_gateways_edit_trigger.php`;
+	}
+	
+	await rp(options)
+		.then(function(resp) {
+			res.json({'html': resp});
+		})
+		.catch(function(err) {
+			res.status(400).json({ message: err })
+		});
+}
+
+async function viewOpnSensePage(req, res, next) {
+	let options = {};
+
+	let { query } = req;
+	if (!query) {
+		res.status(400).json({ message: "Parameter rte is required." });
+	}
+	let { if: itf, rte, id, height } = query; 
+	
+	options.method = 'GET';
+	/*
+	if (!rte) {
+		res.status(400).json({ message: "Parameter rte is required." });
+	}
+	if (itf && id) {
+		options.uri = customHeaders.uri + `${rte}_api.php?if=${itf}&id=${id}`;
+	}
+	else if (id) {
+		options.uri = customHeaders.uri + `${rte}_api.php?if=${itf}&id=${id}`;
+	}
+	else if (itf) {
+		options.uri = customHeaders.uri + `${rte}_api.php?if=${itf}`;
+	} else {
+		options.uri = customHeaders.uri + `${rte}_api.php`;
+	}
+	*/
+	options.uri = `${customHeaders.uri}${rte}?hidemenu=true`;
+	await rp(options)
+		.then(function(resp) {
+//			res.json({'success': true, 'html': resp});
+//			res.set('Content-Type', 'text/html');
+		//	res.send(new Buffer(resp));
+			if (!height) {
+				res.json({'success': true, 'html': '<iframe style="border-width: 0px;" width="100%" height="700" src="' + options.uri + '"></iframe>'});
+			}
+			else {
+				res.json({'success': true, 'html': `<iframe style="border-width: 0px;" width="100%" height="${height}" src="${options.uri}"></iframe>`});
+			}
+//			res.json({'success': true, 'path': options.uri});
+		})
+		.catch(function(err) {
+			res.status(400).json({ message: err })
+		});
+}
+
+async function editOpnSenseItem(req, res, next) {
+	let options = {};
+	let { query } = req;
+	if (!query) {
+		res.status(400).json({ message: "Parameter rte is required." });
+	}
+	let { if: itf, rte, id } = query; 
+	
+	options.method = 'POST';
+	if (!rte) {
+		res.status(400).json({ message: "Parameter rte is required." });
+	}
+	if (itf && id) {
+		options.uri = customHeaders.uri + `${rte}_api.php?if=${itf}&id=${id}`;
+	}
+	else if (id) {
+		options.uri = customHeaders.uri + `${rte}_api.php?if=${itf}&id=${id}`;
+	}
+	else if (itf) {
+		options.uri = customHeaders.uri + `${rte}_api.php?if=${itf}`;
+	} else {
+		options.uri = customHeaders.uri + `${rte}_api.php`;
+	}
+	options.formData = req.body;
+	await rp(options)
+		.then(function(resp) {
+			//res.json(JSON.parse(resp));
+			res.json({'success': true, 'html': resp});
+		})
+		.catch(function(err) {
+			console.log('failure');
+			res.status(400).json({ message: err })
+		});
+}
+
+async function editOpnSenseItemForm(req, res, next) {
+	let options = {};
+	let { query } = req;
+	let { if: itf, rte, id } = query; 
+	
+	options.method = 'GET';
+	if (!rte) {
+		res.status(400).json({ message: "Parameter rte is required." });
+	}
+	if (itf && id) {
+		options.uri = customHeaders.uri + `${rte}_api.php?if=${itf}&id=${id}`;
+	}
+	else if (id) {
+		options.uri = customHeaders.uri + `${rte}_api.php?if=${itf}&id=${id}`;
+	}
+	else if (itf) {
+		options.uri = customHeaders.uri + `${rte}_api.php?if=${itf}`;
+	} else {
+		options.uri = customHeaders.uri + `${rte}_api.php`;
+	}
+	
+	await rp(options)
+		.then(function(resp) {
+			res.json({'html': resp});
+		})
+		.catch(function(err) {
+			res.status(400).json({ message: err })
+		});
+}
+
 async function editInterface(req, res, next) {
-	console.log(req.body);
 	let options = {};
 	options.method = 'POST';
 	options.uri = customHeaders.uri + `/interfaces_trigger.php`;
